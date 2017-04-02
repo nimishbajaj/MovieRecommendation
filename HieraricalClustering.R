15531d7a999e4ceafbfef4cf3dd5be715057413f
Nimish <nimishbajaj@hotmail.com>
2017-04-02 18:18:03 +0530

    Clustering Data

--- HieraricalClustering.R
+++ HieraricalClustering.R
@@ -11,3 +11,19 @@ movies$IMDB = NULL
 movies = unique(movies)
 
 #Clustering Data
+distances = dist(movies[2:20], method="euclidean")
+clusterMovies = hclust(distances, method="ward.D")
+
+#plot dendogram
+plot(clusterMovies)
+
+#Looking at the dendogram and the need of the application
+#we believe that around 10 clusters will appropriately define
+#clustering so
+clusterGroups = cutree(clusterMovies, k = 10)
+
+#Now each entry in the data set is labelled to be belonging to one of these clusters
+#We can name the clusters as well
+
+tapply(movies$Action, clusterGroups, mean)
+
